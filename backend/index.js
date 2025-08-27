249835390674548146c5a86bb2aada938c473874
Surajsa19 <152869815+Surajsa19@users.noreply.github.com>
2025-08-27 14:54:57 +0530

    Update cors

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
 
 app.use("/api/v1", mainRouter);
 
